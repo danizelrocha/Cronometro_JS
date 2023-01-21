ca28c680d2d89bb8c31b4b34dd6009659fc0dbe6
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-01-21 11:46:09 -0300

    Cronômetro
    Finalizado

--- js/scripty.js
+++ js/scripty.js
@@ -1,33 +1,45 @@
 var sec=0
 var min=0
+var hou=0
+
 var interval
 
 
+
+function twoDigits(digit){
+    if(digit<10){
+        return('0'+digit)
+    }else{
+        return(digit)
+    }
+}
+
 function start(){
-  interval=setInterval(watch,10)
-    /* console.log('iniciou') */
+    watch()  // QUEBRA O DELAY DE 1 SEGUNDO NO INICIO DO CRONÔMETRO
+    interval=setInterval(watch,1000)
 }
 
 function pause(){
     clearInterval(interval)
-    /* console.log('Pausou') */
 }
 
 function stop(){
     clearInterval(interval)
     sec=0
     min=0
-    document.getElementById('couter').innerText='00:00'
-
-    /* console.log('Parou') */
+    hou=0
+    document.getElementById('couter').innerText='00:00:00'
 }
 
-function watch(){
-    sec++
+function watch(){                                                                                                 
+    sec++ // ACRESENTOU MAIS UM SEGUNDO NO INICIO DA (FUNCTION STAR) QUEBRANDO O DELAY DE UM SEGUNDO PARA O START.VIDE PRIMEIRO COMENTARIO
     if(sec==60){
         min++
         sec=0
+        if(min==60){
+            min=0
+            hou++
+        }
     }
-    document.getElementById('couter').innerText=min+':'+sec
-    /* console.log('já') */
+    document.getElementById('couter').innerText=twoDigits(hou)+':'+twoDigits(min)+':'+twoDigits(sec);
 }
